b437591bf011fa92c3c98fa41479f80c43495331
Bidisha Das <officialbidisha1@gmail.com>
2022-03-05 01:04:10 +0530

    Added dynamic caching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -7,7 +7,7 @@ if(!window.Promise){
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register(
-            "./../../sw.js"
+            "/sw.js"
         )
         .then(() => {
             console.log('Service worker registered');
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,60 @@
-self.addEventListener('install', function(event){
-       // console.log('[Service Worker] Installing service worker...', event);
-})
 
-self.addEventListener('activate', function(event){
-//     console.log('[Activated Service Worker] Activated service worker...', event);
-    return self.clients.claim();
-})
 
-self.addEventListener('fetch', function(event){
-       // console.log('[Service worker] Fetching something ....', event);
-       event.respondWith(fetch(event.request));
-})
+self.addEventListener('install', function (event) {
+       console.log('[Service Worker] Installing Service Worker ...', event);
+       event.waitUntil(
+              caches.open('static')
+                     .then(function (cache) {
+                            console.log('[Service Worker] Precaching App Shell');
+                            cache.addAll([
+                                   '/',
+                                   '/index.html',
+                                   '/src/js/app.js',
+                                   '/src/js/feed.js',
+                                   '/src/js/promise.js',
+                                   '/src/js/fetch.js',
+                                   '/src/js/material.min.js',
+                                   '/src/css/app.css',
+                                   '/src/css/feed.css',
+                                   '/src/images/main-image.jpg',
+                                   'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                                   'https://fonts.googleapis.com/icon?family=Material+Icons',
+                                   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                            ]);
+                     })
+       )
+});
+
+self.addEventListener('activate', function (event) {
+       console.log('[Service Worker] Activating Service Worker ....', event);
+       return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+       event.respondWith(
+              caches.match(event.request)
+                     .then(function (response) {
+                            if (response) {
+                                   return response;
+                            } else {
+                                   return fetch(event.request)
+                                          .then(function (res) {
+                                                 return caches.open('dynamic')
+                                                        .then(function (cache) {
+                                                               /**
+                                                                * Put just stores the data you have
+                                                                * and do not send any http request
+                                                                * unlike add
+                                                                */
+                                                               cache.put(event.request.url, res.clone())
+                                                               return res;
+                                                        })
+                                          })
+                                          .catch((err)=>{
+                                                        
+                                          })
+                            }
+                     })
+
+       );
+});
