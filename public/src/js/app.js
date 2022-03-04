93615f4d58a6a0074aad9e466848515c16ff3178
Bidisha Das <officialbidisha1@gmail.com>
2022-03-04 22:19:35 +0530

    Added static content

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,14 @@
 var deferredPrompt;
 
+if(!window.Promise){
+     window.Promise = Promise
+}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register("./sw.js")
+        .register(
+            "./../../sw.js"
+        )
         .then(() => {
             console.log('Service worker registered');
         });
@@ -16,3 +22,15 @@ window.addEventListener('beforeinstallpromt', function (event) {
     deferredPrompt = event;
     return false;
 })
+
+// fetch('https://httpbin.org/ip')
+// .then(function(response){
+//      console.log(response);
+//      return response.json()
+// })
+// .then(function(data){
+//     console.log(data)
+// })
+// .catch(function(err){
+//      console.log(err);
+// })
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('install', function(event){
-       console.log('[Service Worker] Installing service worker...', event);
+       // console.log('[Service Worker] Installing service worker...', event);
 })
 
 self.addEventListener('activate', function(event){
-    console.log('[Activated Service Worker] Activated service worker...', event);
+//     console.log('[Activated Service Worker] Activated service worker...', event);
     return self.clients.claim();
 })
 
 self.addEventListener('fetch', function(event){
-       console.log('[Service worker] Fetching something ....', event);
+       // console.log('[Service worker] Fetching something ....', event);
        event.respondWith(fetch(event.request));
 })
